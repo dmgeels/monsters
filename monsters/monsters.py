a2a9f7f304b963c1bad1b61198a097e91e02677b
Dennis Geels <37451614+dmgeels@users.noreply.github.com>
2019-02-20 18:51:56 -0800

    Added rotation, ghosting (broken).

--- monsters/monsters.py
+++ monsters/monsters.py
@@ -12,10 +12,15 @@ class MonsterGame(arcade.Window):
         super().__init__(width, height, title='monsters')
 
         arcade.set_background_color(arcade.color.AMAZON)
+        self.angle_delta = 1
 
     def setup(self):
-        # Set up your game here
-        pass
+        """ One-time setup """
+        self.sprite = arcade.Sprite('img/character.png', scale=0.5, center_x=200, center_y=300)
+        self.sprite.angle = 0
+        self.sprite.alpha = 50
+        self.sprites = arcade.SpriteList(use_spatial_hash=False)
+        self.sprites.append(self.sprite)
 
     def quit(self):
         """ Exit the game """
@@ -24,20 +29,22 @@ class MonsterGame(arcade.Window):
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
-        sprite = arcade.Sprite('img/character.png', scale=0.2, center_x=200, center_y=300)
-        sprites = arcade.SpriteList()
-        sprites.append(sprite)
-        sprites.draw()
-
+        arcade.draw_text('MONSTERS', 140, 200, arcade.color.BLACK, 18)
+        self.sprites.draw()
 
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
-        pass
+        self.sprite.angle += self.angle_delta
 
     def on_key_press(self, key, modifiers):
         """ Handles keyboard. Quits on 'q'. """
         if key == arcade.key.Q:
             self.quit()
+        elif key == arcade.key.SPACE:
+            self.angle_delta *= -1
+        elif key == arcade.key.G:
+            self.sprite.alpha = 300 - self.sprite.alpha
+            print('Ghost!', self.sprite.alpha)
         arcade.set_background_color(arcade.color.BLUE_GRAY)
 
     def on_key_release(self, key, modifiers):
