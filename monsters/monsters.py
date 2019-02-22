00d79f98aac87676e2fb8d05233ab2d1da097fa3
Dennis Geels <37451614+dmgeels@users.noreply.github.com>
2019-02-21 20:35:59 -0800

    Fix ghost mode. Add 'S' key for show-points.

--- monsters/monsters.py
+++ monsters/monsters.py
@@ -13,12 +13,13 @@ class MonsterGame(arcade.Window):
 
         arcade.set_background_color(arcade.color.AMAZON)
         self.angle_delta = 1
+        self.show_points = False
 
     def setup(self):
         """ One-time setup """
         self.sprite = arcade.Sprite('img/character.png', scale=0.5, center_x=200, center_y=300)
         self.sprite.angle = 0
-        self.sprite.alpha = 50
+        self.sprite.alpha = 1.0
         self.sprites = arcade.SpriteList(use_spatial_hash=False)
         self.sprites.append(self.sprite)
 
@@ -30,6 +31,9 @@ class MonsterGame(arcade.Window):
         """ Render the screen. """
         arcade.start_render()
         arcade.draw_text('Dowsha', 140, 200, arcade.color.BLACK, 18)
+        if self.show_points:
+            for x, y in self.sprite.get_points():
+                arcade.draw_point( x, y, arcade.color.BLACK, 2 )
         self.sprites.draw()
 
     def update(self, delta_time):
@@ -43,8 +47,10 @@ class MonsterGame(arcade.Window):
         elif key == arcade.key.SPACE:
             self.angle_delta *= -1
         elif key == arcade.key.G:
-            self.sprite.alpha = 300 - self.sprite.alpha
+            self.sprite.alpha = 1.2 - self.sprite.alpha
             print('Ghost!', self.sprite.alpha)
+        elif key == arcade.key.S:
+            self.show_points = not self.show_points
         arcade.set_background_color(arcade.color.BLUE_GRAY)
 
     def on_key_release(self, key, modifiers):
