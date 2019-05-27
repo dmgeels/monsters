2ae90add4015970fa459c512cc94f2695635f928
Catphire <47507427+Catphire@users.noreply.github.com>
2019-05-27 15:42:48 -0700

    start of Level text

--- monsters/monsters.py
+++ monsters/monsters.py
@@ -498,7 +498,7 @@ class MonsterGame(arcade.Window):
             arcade.draw_text('PAUSED', 270, 300, color=arcade.color.WHITE, font_size=100)
         elif self.board.finished == True:
             arcade.set_background_color((145, 191, 179))
-            arcade.draw_text('Success! You Finished Level' + map_number + 'In '+ str(int(self.board.end_time - self.start_time)) +
+            arcade.draw_text('Success! You Finished Level' + self.board_number + 'In '+ str(int(self.board.end_time - self.start_time)) +
             ' Seconds.', 260, 612, color=arcade.color.BUD_GREEN, font_size=24)
         else:
             arcade.draw_text('Monsters', 410, 612, color=arcade.color.BUD_GREEN, font_size=24)
@@ -507,6 +507,7 @@ class MonsterGame(arcade.Window):
         arcade.draw_text('Press 1-9 For Different levels Or q To Quit', 2, 612, color=arcade.color.WHITE, font_size=10)
         arcade.draw_text('Use Arrow Keys To Move And Space To Shoot', 2, 625, color=arcade.color.WHITE, font_size=10)
         arcade.draw_text('Press ESC To Pause', 820, 615, color=arcade.color.WHITE, font_size=10)
+        arcade.draw_text('Level'+ self.board_number, 410, 20, color=arcade.color.WHITE, font_size=10)
 
 
 
